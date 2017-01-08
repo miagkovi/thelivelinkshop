2c933f027438e011d8cc2eb4472028dfcdeaa0a9
ievgenii <miagkovi@gmail.com>
2017-01-09 00:15:53 +0200

    added new features

---
+++ scripts/main_menu.js
@@ -0,0 +1,7 @@
+$("document").ready(function() {
+	$("#contextButton").on("click", function() {
+		var selectedOption = $( "#select option:selected" );
+		var fulldropToShow = "#fulldrop_" + selectedOption.val();
+		$(fulldropToShow).toggle("slow");
+	});
+});
