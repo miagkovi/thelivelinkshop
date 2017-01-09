40825b8a55f64192d0fcd482c44ab7d114ff15da
ievgenii <miagkovi@gmail.com>
2017-01-09 13:57:39 +0200

    another revision

--- scripts/app.js
+++ scripts/app.js
@@ -27,46 +27,77 @@
 	// }
 // }())
 
-	var specialOffers = document.getElementById("specialOffers");
-	var bestsellers = document.getElementById("bestsellers");
-	var latests = document.getElementById("latests");
-	var soonests = document.getElementById("soonests");
+// 	var specialOffers = document.getElementById("specialOffers");
+// 	var bestsellers = document.getElementById("bestsellers");
+// 	var latests = document.getElementById("latests");
+// 	var soonests = document.getElementById("soonests");
 
-	var specialOffersBtnRight = document.getElementById("specialOffersBtnRight");
-	var specialOffersBtnLeft = document.getElementById("specialOffersBtnLeft");
+// 	var specialOffersBtnRight = document.getElementById("specialOffersBtnRight");
+// 	var specialOffersBtnLeft = document.getElementById("specialOffersBtnLeft");
 
-	specialOffers.style.marginLeft,
-	bestsellers.style.marginLeft,
-	latests.style.marginLeft,
-	soonests.style.marginLeft = "0";
+// 	specialOffers.style.marginLeft,
+// 	bestsellers.style.marginLeft,
+// 	latests.style.marginLeft,
+// 	soonests.style.marginLeft = "0";
 
-var app = {
-	increment: 230,
-	margin: 0,
-	right: function() {
-		if (this.margin > -2300/2) {
-			this.margin = this.margin - this.increment;
-			specialOffers.style.marginLeft = this.margin + "px";
-			// if (this.margin === -100) {
-			// 	specialOffersBtnRight.classList.remove("arrow-fefe-r-a");
-			// 	specialOffersBtnRight.classList.add("arrow-fefe-r");
-			// };
-		} else {
-			this.margin = this.margin;
-		};
-	},
-	left: function() {
-		if (this.margin < 0) {
-			this.margin = this.margin + this.increment;
-			specialOffers.style.marginLeft = this.margin + "px";
-			// if (tis.margin !== 0) {
-			// 	specialOffersBtnLeft.classList.remove("arrow-fefe-l");
-			// 	specialOffersBtnLeft.classList.add("arrow-fefe-l-a");
-			// };
-		} else {
-			this.margin = this.margin;
-		};
-	}
+// var app = {
+// 	increment: 230,
+// 	margin: 0,
+// 	right: function() {
+// 		if (this.margin > -2300/2) {
+// 			this.margin = this.margin - this.increment;
+// 			specialOffers.style.marginLeft = this.margin + "px";
+// 			// if (this.margin === -2300/2) {
+// 			// 	specialOffersBtnRight.classList.remove("arrow-fefe-r-a");
+// 			// 	specialOffersBtnRight.classList.add("arrow-fefe-r");
+// 			// };
+// 		} else {
+// 			this.margin = this.margin;
+// 		};
+// 	},
+// 	left: function() {
+// 		if (this.margin < 0) {
+// 			this.margin = this.margin + this.increment;
+// 			specialOffers.style.marginLeft = this.margin + "px";
+// 			// if (this.margin !== 0) {
+// 			// 	specialOffersBtnLeft.classList.remove("arrow-fefe-l");
+// 			// 	specialOffersBtnLeft.classList.add("arrow-fefe-l-a");
+// 			// };
+// 		} else {
+// 			this.margin = this.margin;
+// 		};
+// 	}
+// };
+
+var App = function(id) {
+	//constructor
+	this.id = document.getElementById(id);
+	this.increment = 230;
+	this.margin = 0;
+	this.id.style.marginLeft = "0";
 };
 
+App.prototype.right = function() {
+	//right move
+	if (this.margin > -2300/2) {
+		this.margin = this.margin - this.increment;
+		this.id.style.marginLeft = this.margin + "px";
+	} else {
+		this.margin = this.margin;
+	};
+};
 
+App.prototype.left = function() {
+	//left move
+	if (this.margin < 0) {
+		this.margin = this.margin + this.increment;
+		this.id.style.marginLeft = this.margin + "px";
+	} else {
+		this.margin = this.margin;
+	};
+};
+
+var specialOffersApp = new App("specialOffers");
+var bestsellersApp = new App("bestsellers");
+var latestsApp = new App("latests");
+var soonestsApp = new App("soonests");
